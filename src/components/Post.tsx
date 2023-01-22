2f643321fc9391bc3cc3fadb7ab587e33b10e4db
LeonardoDias <66549962+LeonardoMarquesDias@users.noreply.github.com>
2023-01-22 00:05:01 -0300

    Post Component - structure

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,6 @@
 import styles from './Post.module.css'
 
+import { Comment } from './Comment';
 
 export function Post() {
   return (
@@ -9,7 +10,7 @@ export function Post() {
           <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/66549962?v=4" />
           <div className={styles.authorInfo}>
             <strong>Leonardo Dias</strong>
-            <span>Blockchain Developer</span>
+            <span>Crypto Blogger</span>
           </div>
         </div>
 
@@ -21,6 +22,24 @@ export function Post() {
         <p>What's Bitcoin?</p>
         <p>Bitcoin is a decentralized digital currency that uses cryptography to secure and verify transactions on its network. It was created in 2009 by an individual or group of individuals using the pseudonym Satoshi Nakamoto. Bitcoin operates without a central authority and can be transferred directly between individuals without the need for intermediaries such as banks. It is based on blockchain technology, which is a decentralized ledger that records all transactions on the network.</p>
         <p><a href="">#bitcoin #blockchain</a></p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Your feedback</strong>
+
+        <textarea 
+          placeholder='your comment...'
+        />
+
+        <footer>
+          <button type="submit">Post Comment</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}> 
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   )
